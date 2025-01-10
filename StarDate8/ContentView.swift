d8073fd16ce335fd3b7f120b4ed34a3c5e435a71
Richard Gagg <r.gagg@outlook.com>
2025-01-11 10:55:36 +1100

    Initial Commit

---
+++ StarDate8/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StarDate8
+//
+//  Created by Richard Gagg on 11/1/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StarDate8/StarDate8App.swift
@@ -0,0 +1,17 @@
+//
+//  StarDate8App.swift
+//  StarDate8
+//
+//  Created by Richard Gagg on 11/1/2025.
+//
+
+import SwiftUI
+
+@main
+struct StarDate8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
